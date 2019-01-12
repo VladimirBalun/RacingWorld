66922884a6b4f1e42b5a814da42f27ca1c1ba41d
TheMrDen3D <den16081988@mail.ru>
2019-01-12 11:19:10 +0300

    Added module to work with simple mesh
    Added module to work with simple mesh
    Changes:
    * Creates simple models (cube, tetrahedron)
    * Uses base shader program for rendering

---
+++ ClientSide/Sources/Graphics/SceneGraph/SimpleMesh.hpp
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 Vladimir Balun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../SceneGraph/VertexBuffers.hpp"
+#include "../Utils/ShaderProgram.hpp"
+
+namespace Graphics { namespace SimpleMesh 
+{
+    enum TypeRender
+    {
+        TR_WIRE  = 0x1B01,
+        TR_SOLID = 0x1B02,
+        TR_POINT = 0x1B00,
+    };
+
+    class SimpleMesh
+    {
+    public:
+        SimpleMesh();
+        void createCube(float size);
+        void createTetrahedron(float size);
+        void drawMesh(TypeRender tr);
+        ~SimpleMesh();
+    private:
+        Graphics::SceneGraph::HardwareBuffers* mesh;
+        Graphics::Utils::ShaderProgram *shader;
+        int positionLocation;
+    };
+ } }
