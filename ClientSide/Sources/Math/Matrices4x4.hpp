34d15834c70a3a793dd50ce64c90f7b130bdf373
TheMrDen3D <den16081988@mail.ru>
2019-01-12 10:03:37 +0300

    Fixed small errors with matrixes
    Fixed small errors with matrixes
    Changes:
    * Fixed small errors with matrices;

--- ClientSide/Sources/Math/Matrices4x4.hpp
+++ ClientSide/Sources/Math/Matrices4x4.hpp
@@ -129,7 +129,7 @@ namespace Math {
     {
         Type rotation[] = {
             cos(angle), -sin(angle), 0.0, 0.0,
-            sin(angle), 1.0,  cos(angle), 0.0,
+            sin(angle),  cos(angle), 0.0, 0.0,
             0.0, 0.0, 1.0, 0.0,
             0.0, 0.0, 0.0, 1.0
         };
