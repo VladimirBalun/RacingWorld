2b916887689455b7db1c322b53c8f51eadf0d575
TheMrDen3D <den16081988@mail.ru>
2019-01-13 16:01:05 +0300

    Implemented module ShaderManager  for managing shader programs
    Changes:
    * Adding a shader program to the manager
    * Remove shader program from the GPU
    * Shader program management by resource name

---
+++ ClientSide/Sources/Graphics/SceneGraph/ShaderManager.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2018 Vladimir Balun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "ShaderManager.hpp"
+#include "../../Utils/Logger.hpp"
+
+
+Graphics::Manager::ShaderManager::ShaderManager()
+{
+    currentShader = "";
+}
+
+Graphics::Manager::ShaderManager::~ShaderManager()
+{
+}
+
+void Graphics::Manager::ShaderManager::addShader(std::string key, const char* vShaderFileName, const char* fShaderFileName)
+{
+    auto res = shadersList.find(key);
+    if (res != shadersList.end())
+    {
+        LOG_PRINT("This Shader Name already exists!");
+    }
+    else
+    {
+        Utils::ShaderProgram newShader(vShaderFileName, fShaderFileName);
+        shadersList.emplace(std::make_pair(key, newShader.getProgram()));
+    }
+}
+
+void Graphics::Manager::ShaderManager::setShader(std::string key)
+{
+    auto res = shadersList.find(key);
+    if (res != shadersList.end())
+    {
+        currentShader = key;
+        mProgram.setProgram(shadersList.at(currentShader));
+    }
+    else 
+    {
+        LOG_PRINT("This shader name does not exist!");
+    }
+}
+
+void Graphics::Manager::ShaderManager::unsetShader()
+{
+    mProgram.unsetProgram();
+}
+
+UINT Graphics::Manager::ShaderManager::getIdShader(std::string key)
+{
+    if (currentShader != key) 
+    {
+        setShader(key);
+    }
+
+    auto res = shadersList.find(key);
+    if (res != shadersList.end()) {
+        return shadersList.at(key);
+    }
+    else 
+    {
+        LOG_PRINT("This Shader does not exist!");
+    }
+    return -1;
+}
+
+void Graphics::Manager::ShaderManager::destroyProgram(std::string key)
+{
+    UINT idProg = getIdShader(key);
+    getShader(key).destroyProgram(idProg);
+    shadersList.erase(key); 
+}
+
+Graphics::Utils::ShaderProgram Graphics::Manager::ShaderManager::getShader(std::string key)
+{
+    setShader(key);
+    return mProgram;
+}
+
+void Graphics::Manager::ShaderManager::PrintShadersList()
+{
+    std::cout << "=========================================" << std::endl;
+    std::cout << "List shaders" << std::endl;
+    std::cout << "-----------------------------------------" << std::endl;
+    for (auto it = shadersList.begin(); it != shadersList.end(); ++it)
+    {
+        std::cout << (*it).first << " " << (*it).second << std::endl;
+    }
+    std::cout << "=========================================" << std::endl;
+}
---
+++ ClientSide/Sources/Graphics/SceneGraph/ShaderManager.hpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Vladimir Balun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "../Utils/ShaderProgram.hpp"
+#include <map>
+
+
+namespace Graphics { namespace Manager {
+    class ShaderManager
+    {
+    public:
+        ShaderManager();
+        virtual ~ShaderManager();
+        void addShader(std::string key, const char* vShaderFileName, const char* fShaderFileName);
+        void setShader(std::string key);
+        void unsetShader();
+        UINT getIdShader(std::string key);
+        void destroyProgram(std::string key);
+        Utils::ShaderProgram getShader(std::string key);
+
+        void PrintShadersList(); // for debugging
+
+    private:
+        std::string currentShader;
+        std::map<std::string, UINT> shadersList;
+        Utils::ShaderProgram mProgram;
+    };
+
+} }
+
