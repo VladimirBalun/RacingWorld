d31deeb5eb3de32d5582bbd7e007f4465ff52b7b
Balun Vladimir <balun2010@mail.ru>
2019-03-09 14:27:58 +0300

    feat(web site): add structure HTTP server for web site
    Was added structure HTTP server for web site.

---
+++ WebSite/server/sources/server.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 Vladimir Balun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const router = require('./router');
+const configuration = require('../resources/configuration/network');
+
+const server = express();
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Credentials', 'true');
+    response.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    next();
+});
+
+const log4js = require('log4js');
+const log = log4js.getLogger(__filename);
+log.level = 'info';
+
+server.listen(configuration.serverPort, configuration.serverAddress, () => {
+    router.route(server);
+    log.info(`Server was started successfully on ${configuration.fullServerAddress}`);
+});
+
+module.exports.server = server;
