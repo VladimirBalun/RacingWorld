2a69f1f276bc0b7aa1a968a3864fa1b41cbe1d3c
Balun Vladimir <balun2010@mail.ru>
2019-03-09 11:09:00 +0300

    ci: update scripts for building server side
    Was updated scripts for building server side.

--- Scripts/BuildServerSide.sh
+++ Scripts/BuildServerSide.sh
@@ -4,8 +4,13 @@ readonly CURRENT_PATH=$(pwd)
 readonly SERVER_SIDE_PATH=$CURRENT_PATH/../ServerSide
 readonly GAME_SERVER_PATH=$SERVER_SIDE_PATH/gameserver
 readonly DATABASE_SERVER_PATH=$SERVER_SIDE_PATH/databaseserver
+readonly PROTOCOL_PATH=$SERVER_SIDE_PATH/protocol
 
 echo "Copyright 2018 Vladimir Balun - Script to build all the servers on Linux."
+
+echo "Building of the protocol between servers..."
+cd $PROTOCOL_PATH
+mvn clean compile assembly:single
 
 echo "Building of the game server..."
 cd $GAME_SERVER_PATH
