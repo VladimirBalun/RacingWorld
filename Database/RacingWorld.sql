da86098d5c93fdfa6f676ec43380b01c43cec1fb
Shelter185 <Shelter185@yandex.ru>
2019-03-14 09:01:42 +0300

    feat(database): add new structure of the database
    Was added new structure of the database with tables(maps, racings, playing_users, ...).

--- Database/RacingWorld.sql
+++ Database/RacingWorld.sql
@@ -16,6 +16,21 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `maps`
+--
+
+DROP TABLE IF EXISTS `maps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `maps` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `news`
 --
 
@@ -23,11 +38,67 @@ DROP TABLE IF EXISTS `news`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `news` (
-  `id_news` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `date` date NOT NULL,
-  PRIMARY KEY (`id_news`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title_UNIQUE` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `playing_users`
+--
+
+DROP TABLE IF EXISTS `playing_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `playing_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) unsigned NOT NULL,
+  `id_running_racing` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `playing_users_with_running_racings_idx` (`id_running_racing`),
+  KEY `playing_users_with_users_idx` (`id_user`),
+  CONSTRAINT `playing_users_with_running_racings` FOREIGN KEY (`id_running_racing`) REFERENCES `running_racings` (`id`),
+  CONSTRAINT `playing_users_with_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `racings`
+--
+
+DROP TABLE IF EXISTS `racings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `racings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `start_time` datetime NOT NULL,
+  `id_map` int(11) unsigned NOT NULL,
+  `count_players` int(11) NOT NULL,
+  `total_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `racings_with_maps_idx` (`id_map`),
+  CONSTRAINT `racings_with_maps` FOREIGN KEY (`id_map`) REFERENCES `maps` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `running_racings`
+--
+
+DROP TABLE IF EXISTS `running_racings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `running_racings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `start_time` datetime NOT NULL,
+  `id_map` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `running_racings_with_maps_idx` (`id_map`),
+  CONSTRAINT `running_racings_with_maps` FOREIGN KEY (`id_map`) REFERENCES `maps` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -39,12 +110,31 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `users` (
-  `id_user` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(254) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id_user`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users_racings`
+--
+
+DROP TABLE IF EXISTS `users_racings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users_racings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) unsigned NOT NULL,
+  `id_racing` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_racings_with_users_idx` (`id_user`),
+  KEY `users_racings_with_racings_idx` (`id_racing`),
+  CONSTRAINT `users_racings_with_racings` FOREIGN KEY (`id_racing`) REFERENCES `racings` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_racings_with_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -56,4 +146,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-03-08 20:27:50
+-- Dump completed on 2019-03-14  9:00:02
