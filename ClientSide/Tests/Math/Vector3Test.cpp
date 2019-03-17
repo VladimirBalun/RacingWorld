f5d52a046334b9e4ac419a2e37822577a496b7e1
Dima <dimazverev716@gmail.com>
2019-03-17 14:06:41 +0300

    feat(client side): add test for Vector3
    Was added test for Vector3 with common operations(addition, subtraction, multiplication, ...).

--- ClientSide/Tests/Math/Vector3Test.cpp
+++ ClientSide/Tests/Math/Vector3Test.cpp
@@ -16,3 +16,39 @@
 
 #define CATCH_CONFIG_MAIN
 #include "Catch.hpp"
+#include "Vectors/Vector3.hpp"
+
+TEST_CASE("Tests for Vector3", "[Vector3]")
+{
+
+	SECTION("Addition of the vectors")
+	{
+		Math::Vector3i first(1, 3, 2);
+		Math::Vector3i second(2, 4, 6);
+		REQUIRE((first + second) == Math::Vector3i(3, 7, 8));
+	}
+
+	SECTION("Subtraction of the vectors")
+	{
+		Math::Vector3i first(2, 3, 5);
+		Math::Vector3i second(1, 2, 3);
+		REQUIRE((first - second) == Math::Vector3i(1, 1, 2));
+	}
+
+	SECTION("Multiplication vector in scalar")
+	{
+		Math::Vector3i vector(1, 3, 4);
+		REQUIRE((vector * 2) == Math::Vector3i(2, 6, 8));
+	}
+
+	SECTION("Converting vector to array")
+	{
+		Math::Vector3i vector(1, 3, 2);
+		int array[Math::Vector3i::VECTOR_SIZE] = { 0 };
+		vector.toArray(array);
+		REQUIRE(vector.getX() == array[0]);
+		REQUIRE(vector.getY() == array[1]);
+		REQUIRE(vector.getZ() == array[2]);
+	}
+
+}
