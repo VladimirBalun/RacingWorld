8484a3e31c17352bf16380a0745ea32df5b5febb
Varexsk <sk.varex@gmail.com>
2019-03-17 14:47:44 +0300

    feat(server side): add new entities for database server
    added entity: Map, PlayingUser, Racing, RacingUser, RunningRaces

---
+++ ServerSide/protocol/src/main/java/ru/servers/protocol/gameserverwithdatabaseserver/entity/Racing.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Vladimir Balun
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ru.servers.protocol.gameserverwithdatabaseserver.entity;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Racing implements Serializable {
+
+    private int id;
+    private Date startTime;
+    private Map map;
+    private int countPlayers;
+    private Date totalTime;
+
+    public Racing(Date startTime, Map map, int countPlayers, Date totalTime){
+        this.startTime = startTime;
+        this.map = map;
+        this.countPlayers = countPlayers;
+        this.totalTime = totalTime;
+    }
+}
