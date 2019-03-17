a3abbedf230ec9ed6022c99980903cbd9b885ba0
Artem <smipe.artem@gmail.com>
2019-03-17 13:58:39 +0500

    feat(server side): add tests for checking point and get volume
    Added tests for checking point in Parallelepiped and get volume.

---
+++ ServerSide/gameserver/src/test/java/ru/servers/gameserver/math/stereometry/ParallelepipedTest.java
@@ -0,0 +1,46 @@
+package ru.servers.gameserver.math.stereometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.servers.gameserver.math.algebra.vectors.Vector3;
+
+public class ParallelepipedTest {
+
+    private final Vector3 points[] = new Vector3[] {  //         *--------*           Z
+            new Vector3(5.0, 0.0, 0.0),      //        /|       /|           |
+            new Vector3(0.0, 0.0, 0.0),      //       / |      / |           |
+            new Vector3(0.0, 10.0, 0.0),     //      /  |     /  |           |
+            new Vector3(5.0, 10.0, 0.0),     //     *--------*   |           |
+            new Vector3(5.0, 0.0, 6.0),      //     |   *----|---*           O--------Y
+            new Vector3(0.0, 0.0, 6.0),      //     |  /     |  /           /
+            new Vector3(0.0, 10.0, 6.0),     //     | /      | /           /
+            new Vector3(5.0, 10.0, 6.0)      //     |/       |/           /
+    };                                                //     *--------*           X
+
+    Parallelepiped parallelepiped = new Parallelepiped(points);
+
+    @Test // height * length * width
+    public void gettingVolume(){
+        final double length = 5.0;
+        final double width = 10.0;
+        final double height = 6.0;
+        final double volume = height * length * width;
+        Assert.assertEquals(volume, parallelepiped.getVolume(),1.0);
+    }
+
+    @Test // // Test1: Check vertex x in Parallelepiped
+    public void checkingVertexOnNonExistingInParallelepiped() {
+        Assert.assertFalse(parallelepiped.isInside(new Vector3(-1.0, -1.0, 0.0)));
+    }
+
+    @Test // // Test2: Check vertex x in Parallelepiped
+    public void checkingVertexOnExistingOnBorderParallelepiped() {
+        Assert.assertTrue(parallelepiped.isInside(new Vector3(3.5, 10.0, 0.0)));
+    }
+
+    @Test // // Test3: Check vertex x in Parallelepiped
+    public void checkingVertexOnExistingInParallelepiped() {
+        Assert.assertTrue(parallelepiped.isInside(new Vector3(2.5, 5.0, 3.0)));
+    }
+
+}
