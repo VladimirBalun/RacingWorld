c90941c4065ed4588197a66348505f4498545b5e
Artem <smipe.artem@gmail.com>
2019-03-24 10:43:13 +0500

    feat(server side): add variable and method in Route
    Was added variable and method in Route.

--- ServerSide/gameserver/src/main/java/ru/servers/gameserver/ecs/components/Route.java
+++ ServerSide/gameserver/src/main/java/ru/servers/gameserver/ecs/components/Route.java
@@ -1,4 +1,26 @@
 package ru.servers.gameserver.ecs.components;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.servers.gameserver.math.algebra.vectors.Vector3;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Route {
+
+    private Vector3 route;
+
+    @Override
+    public Route clone() {
+        try {
+            return (Route)super.clone();
+        }
+        catch (CloneNotSupportedException e) {
+            throw new InternalError();
+        }
+    }
+
 }
