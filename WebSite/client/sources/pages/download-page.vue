bf257fa39ba331faa0f9a61ddf2c413221eb8287
Anton <Korolewsky.anton3@gmail.com>
2019-04-22 21:28:07 +0300

    feat(web site): changing positioning and added matching html and css
    changed the positioning of objects on the page, the code is changed to conform to the standards of Google HTML / CSS Style Guide and w3.org CSS / HTML validators

--- WebSite/client/sources/pages/download-page.vue
+++ WebSite/client/sources/pages/download-page.vue
@@ -15,52 +15,55 @@
 -->
 
 <template>
-    <div id="app">
-        <div class="main-container">
-            <section class="info">
-                <b>Минимальные системные требования</b>
-                <ul>
-                    <li>CPU:     Pentium III or AMD K6</li>
-                    <li>RAM:     100MB</li>
-                    <li>OpenGL:  Version 3.3</li>
-                    <li>OS:	 Windows 7, Windows 8, Windows 10</li>
-                    <li>Store:	 500MB available space</li>
-                    <li>Network: Broadband Internet connection</li>
-                </ul><br>
-                <b>Рекомендуемые системные требования</b>
-                <ul>
-                    <li>CPU:     Pentium III or AMD K6</li>
-                    <li>RAM:     100MB</li>
-                    <li>OpenGL:  Version 3.3</li>
-                    <li>OS:	 Windows 7, Windows 8, Windows 10</li>
-                    <li>Store:	 500MB available space</li>
-                    <li>Network: Broadband Internet connection</li>
-                </ul>
-            </section>
-            <section class="dwlogo">
-                <section class="logo">
-                    <img src="https://github.com/VladimirBalun/RacingWorld/raw/develop/RacingWorld.png">
-                </section>
-                <section class="dw-btns">
-                    <b>32-bit версия</b>
-                    <div class="btn-container">
-                        <div class="wrap-btn-container">
-                            <div class="btn-bg"></div>
-                            <button class="dwn-btn" @click="get32">Скачать</button>
-                        </div>
-                    </div>
-                    <b>64-bit версия</b>
-                    <div class="btn-container">
-                        <div class="wrap-btn-container">
-                            <div class="btn-bg"></div>
-                            <button class="dwn-btn" @click="get64">Скачать</button>
-                        </div>
-                    </div>
-                </section>
-            </section>
-        </div>
+  <div id="download-webpage">
+    <div class="main-container">
+      <section class="info">
+        <h2 hidden>Информация и системные требования</h2>
+        <h4><b>Минимальные системные требования</b></h4>
+          <ul>
+            <li>CPU: Pentium III or AMD K6</li>
+            <li>RAM: 100MB</li>
+            <li>OpenGL: Version 3.3</li>
+            <li>OS: Windows 7, Windows 8, Windows 10</li>
+            <li>Store: 500MB available space</li>
+            <li>Network: Broadband Internet connection</li>
+          </ul><br>
+          <h4><b>Рекомендуемые системные требования</b></h4>
+          <ul>
+            <li>CPU: Pentium III or AMD K6</li>
+            <li>RAM: 100MB</li>
+            <li>OpenGL: Version 3.3</li>
+            <li>OS: Windows 7, Windows 8, Windows 10</li>
+            <li>Store: 500MB available space</li>
+            <li>Network: Broadband Internet connection</li>
+          </ul>
+      </section>
+      <section class="download-logo">
+          <h2 hidden>Логотип и кнопки загрузки</h2>
+        <section class="logo">
+          <h2 hidden>Логотип продукта</h2>
+          <img src="//github.com/VladimirBalun/RacingWorld/raw/develop/RacingWorld.png" alt="Logo">
+        </section>
+        <section class="download-btns">
+          <h2 hidden>Кнопки для загрузки</h2>
+          <b>32-bit версия</b>
+          <div class="btn-container">
+            <div class="wrap-btn-container">
+              <div class="btn-bg"></div>
+              <button class="download-btn" @click="get32">Скачать</button>
+            </div>
+          </div>
+          <b>64-bit версия</b>
+            <div class="btn-container">
+              <div class="wrap-btn-container">
+                <div class="btn-bg"></div>
+                <button class="download-btn" @click="get64">Скачать</button>
+              </div>
+            </div>
+        </section>
+      </section>
     </div>
-
+  </div>
 </template>
 
 <script>
@@ -90,10 +93,6 @@
 
 <style scoped lang="sass">
 
-    font-face
-        font-family: sans-serif
-        src: url("https://fonts.googleapis.com/css?family=Roboto")
-
     button:hover
         cursor: pointer
 
@@ -102,12 +101,12 @@
         border: none
         background: transparent
 
-    #app
+    #download-webpage
         display: flex
-        display: -webkit-box
-        display: -webkit-flex
         display: -moz-box
         display: -ms-flexbox
+        display: -webkit-box
+        display: -webkit-flex
         justify-content: center
         box-sizing: border-box
 
@@ -117,22 +116,22 @@
         transition: all 0.2s
         width: 1024px
         margin: 40px 0
-        padding: 20px
-        display: -webkit-box
-        display: -webkit-flex
+        padding: 40px 40px 20px 40px
         display: -moz-box
         display: -ms-flexbox
+        display: -webkit-box
+        display: -webkit-flex
         display: flex
         justify-content: center
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2), -5px -5px 10px rgba(0, 0, 0, 0.1)
+        box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2), -5px -5px 10px rgba(0, 0, 0, 0.1)
         font: 300 16px/22px "Roboto", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
 
     .info
-        width: 60%
-        height: 500px
-        margin: 0 2%
-        padding: 30px 0px 30px 77px
-        text-align: left
+        box-sizing: border-box
+        width: 50%
+        height: 550px
+        padding: 10px 0 20px 4%
+        margin: 0 20px 0 0
 
     .info b
         line-height: 33px
@@ -140,35 +139,39 @@
     .info li
         padding: 3px
 
-    .dwlogo
-        width: 30%
+    .download-logo
+        display: block
+        width: 40%
         height: 500px
+
+    .logo
+        width: 100%
+        height: 40%
 
     .logo img
         width: 100%
 
-    .dw-btns
+    .download-btns
         box-sizing: border-box
-        width: 100%
-        height: 65%
+        width: 90%
+        height: 60%
         border-radius: 10px
         overflow: hidden
-        padding: 20px 5px
-        margin-top: 10px
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.1)
+        padding: 20px 0 0 40px
+        margin: 10px 231px 0 0
         text-align: center
         line-height: 44px
         font-weight: 500
 
     .btn-container
-        display: -webkit-box
-        display: -webkit-flex
         display: -moz-box
         display: -ms-flexbox
+        display: -webkit-box
+        display: -webkit-flex
         display: flex
         flex-wrap: wrap
         justify-content: center
-        padding: 5px 0px 35px 0px
+        padding: 5px 0 35px 0
 
     .wrap-btn-container
         width: 100%
@@ -185,10 +188,10 @@
         width: 300%
         height: 100%
         background: #a64bf4
-        background: -webkit-linear-gradient(right, #ff0100, #2c0000, #ff0100, #2c0000)
-        background: -o-linear-gradient(right, #ff0100, #2c0000, #ff0100, #2c0000)
-        background: -moz-linear-gradient(right, #ff0100, #2c0000, #ff0100, #2c0000)
-        background: linear-gradient(right, #ff0100, #2c0000, #ff0100, #2c0000)
+        background: -o-linear-gradient(to right, #ff0100, #2c0000, #ff0100, #2c0000)
+        background: -moz-linear-gradient(to right, #ff0100, #2c0000, #ff0100, #2c0000)
+        background: -webkit-linear-gradient(to right, #ff0100, #2c0000, #ff0100, #2c0000)
+        background: linear-gradient(to right, #ff0100, #2c0000, #ff0100, #2c0000)
         top: 0
         left: -100%
         -webkit-transition: all 0.4s
@@ -196,7 +199,7 @@
         -moz-transition: all 0.4s
         transition: all 0.4s
 
-    .dwn-btn
+    .download-btn
         font-size: 15px
         color: #fff
         line-height: 1.2
@@ -215,6 +218,5 @@
 
     .wrap-btn-container:hover .btn-bg
         left: 0
-
 
 </style>
