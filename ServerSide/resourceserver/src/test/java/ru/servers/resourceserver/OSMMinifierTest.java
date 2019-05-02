e711e6ab868590387a50c1463b5e713f04cd7911
Artem <smipe.artem@gmail.com>
2019-05-02 09:47:20 +0500

    refactor(server side): deleted extra comments, changed exception class
    Was deleted extra comments, changed exception class, deleted empty lines and superfluous data normalization method.

--- ServerSide/resourceserver/src/test/java/ru/servers/resourceserver/OSMMinifierTest.java
+++ ServerSide/resourceserver/src/test/java/ru/servers/resourceserver/OSMMinifierTest.java
@@ -8,9 +8,8 @@ public class OSMMinifierTest {
 
     private OSMMinifier osmMinifier = new OSMMinifier();
 
-    @Test // deleting attributes in tag node
+    @Test
     public void checkingNodeMinification() throws InvalidOSMDataException {
-
          String sourceData =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
             "<osm attribution=\"http://www.openstreetmap.org/copyright\" copyright=\"OpenStreetMap and contributors\" generator=\"CGImap 0.6.1 (11633 thorn-03.openstreetmap.org)\" license=\"http://opendatacommons.org/licenses/odbl/1-0/\" version=\"0.6\">" +
@@ -23,14 +22,13 @@ public class OSMMinifierTest {
             "</osm>";
 
         String resultMinification = osmMinifier.minify(sourceData);
-        minifiedData = normalizationMinifiedData(minifiedData);
-        resultMinification = normalizationResultMinification(resultMinification);
+        minifiedData = normalizationData(minifiedData);
+        resultMinification = normalizationData(resultMinification);
         Assert.assertEquals(minifiedData, resultMinification);
     }
 
-    @Test // deleting attributes in tag way
+    @Test
     public void checkingWayMinification() throws InvalidOSMDataException {
-
         String sourceData =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
                         "<osm attribution=\"http://www.openstreetmap.org/copyright\" copyright=\"OpenStreetMap and contributors\" generator=\"CGImap 0.6.1 (11633 thorn-03.openstreetmap.org)\" license=\"http://opendatacommons.org/licenses/odbl/1-0/\" version=\"0.6\">" +
@@ -43,14 +41,13 @@ public class OSMMinifierTest {
                         "</osm>";
 
         String resultMinification = osmMinifier.minify(sourceData);
-        minifiedData = normalizationMinifiedData(minifiedData);
-        resultMinification = normalizationResultMinification(resultMinification);
+        minifiedData = normalizationData(minifiedData);
+        resultMinification = normalizationData(resultMinification);
         Assert.assertEquals(minifiedData, resultMinification);
     }
 
-    @Test // deleting attributes in tag relation
+    @Test
     public void checkingRelationMinification() throws InvalidOSMDataException {
-
         String sourceData =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
                         "<osm attribution=\"http://www.openstreetmap.org/copyright\" copyright=\"OpenStreetMap and contributors\" generator=\"CGImap 0.6.1 (11633 thorn-03.openstreetmap.org)\" license=\"http://opendatacommons.org/licenses/odbl/1-0/\" version=\"0.6\">" +
@@ -63,45 +60,34 @@ public class OSMMinifierTest {
                         "</osm>";
 
         String resultMinification = osmMinifier.minify(sourceData);
-        minifiedData = normalizationMinifiedData(minifiedData);
-        resultMinification = normalizationResultMinification(resultMinification);
+        minifiedData = normalizationData(minifiedData);
+        resultMinification = normalizationData(resultMinification);
         Assert.assertEquals(minifiedData, resultMinification);
     }
 
-    @Test(expected = InvalidOSMDataException.class) // minification invalid string osmData
+    @Test(expected = InvalidOSMDataException.class)
     public void checkingInvalidStringMinification() throws InvalidOSMDataException {
-
         final String sourceData =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?> " +
                         "<osm attribution=\"http://www.openstreetmap.org/copyright\" copyright=\"OpenStreetMap and contributors\" generator=\"CGImap 0.6.1 (11633 thorn-03.openstreetmap.org)\" license=\"http://opendatacommons.org/licenses/odbl/1-0/\" version=\"0.6\">" +
                         "<way id=\"580429971\" visible=\"true\" version=\"1\" changeset=\"58135270\" timestamp=\"2018-04-16T11:43:32Z\" user=\"Alex_Enot\" uid=\"5757927\"></way>";
-        final String minifiedData = "";
 
         String resultMinification = osmMinifier.minify(sourceData);
-        Assert.assertEquals(minifiedData, resultMinification);
+        Assert.assertNull(resultMinification);
     }
 
-    @Test(expected = InvalidOSMDataException.class) // minification empty string osmData
+    @Test(expected = InvalidOSMDataException.class)
     public void checkingEmptyStringMinification() throws InvalidOSMDataException {
-
         final String sourceData = "";
-        final String minifiedData = "";
 
         String resultMinification = osmMinifier.minify(sourceData);
-        Assert.assertEquals(minifiedData, resultMinification);
+        Assert.assertNull(resultMinification);
     }
 
-    private String normalizationMinifiedData(String minifiedData) {
-        minifiedData = minifiedData.replaceAll("\\s", "");
-        minifiedData = minifiedData.replaceAll("\\t", "");
-        minifiedData = minifiedData.replaceAll("\\n", "");
-        return minifiedData;
-    }
-
-    private String normalizationResultMinification(String resultMinification) {
-        resultMinification = resultMinification.replaceAll("\\s","");
-        resultMinification = resultMinification.replaceAll("\\t","");
-        resultMinification = resultMinification.replaceAll("\\n","");
-        return resultMinification;
+    private String normalizationData(String osmData) {
+        osmData = osmData.replaceAll("\\s", "");
+        osmData = osmData.replaceAll("\\t", "");
+        osmData = osmData.replaceAll("\\n", "");
+        return osmData;
     }
 }
